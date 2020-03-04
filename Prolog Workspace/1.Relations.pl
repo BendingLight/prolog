2121017a221981c5b59cef940c0286927d928835
Roy <42151542+BendingLight@users.noreply.github.com>
2020-03-04 23:28:15 +0530

    Add files via upload

---
+++ Prolog Workspace/1.Relations.pl
@@ -0,0 +1,11 @@
+% Relations or Predicate or Clauses
+
+teaches('sakshi','algo').
+teaches('sakshi','graphics'.
+teaches('arun','data science').
+teaches('arun','machine learning').
+
+studies('ankur','algo').
+studies('ankur','graphics').
+studies('nitesh','geometry').
+studies('nitesh','data science').
